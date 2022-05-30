d2eeab1ce8a83a69df337ace8f4c626a0df47791
hudnellmarcus <hudnell.marcus@gmail.com>
2022-05-30 13:02:02 -0700

    server setup and file structure

---
+++ server.js
@@ -0,0 +1,29 @@
+// Dependencies 
+require('dotenv').config();
+const express = require('express');
+const app = express(); 
+
+
+
+
+
+
+// Middleware 
+
+
+
+
+// Routes / Controllers 
+
+
+
+
+// Listener 
+PORT = process.env.PORT
+
+app.listen(PORT, () => {
+    console.log(`Listening to Andre ${PORT}`);
+})
+
+
+
